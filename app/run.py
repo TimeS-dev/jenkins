d8c931a64a2277622bcdf884860db115015636a9
Timur Sarantsev <3317226@gmail.com>
2022-03-08 02:19:46 +0300

    app

--- app/run.py
+++ app/run.py
@@ -3,8 +3,8 @@ from flask import Flask
 import os
 build = os.environ['env_build']
 app = Flask(__name__)
-@app.route("/")
-def hello_world():
+@app.route('/', methods=['POST', 'GET'])
+def home():
     return "<p>Hello, World!<p>build number: "+build
 if __name__=="__main__":
-    app.run(debug=True, host="0.0.0.0", port=80)
+    app.run(debug=True, host='0.0.0.0', port='5000')
